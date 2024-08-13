4adbc3d4249ac99a4135f93f6db2bf32790dcd00
Diego-Layton <146788313+Diego-Layton@users.noreply.github.com>
2024-08-12 19:27:02 -0500

    Update cronClientes.js

--- controllers/cronClientes.js
+++ controllers/cronClientes.js
@@ -10,10 +10,11 @@ const authToken = process.env.TWILIO_AUTH_TOKEN;
 // const accountSid="ACc4e57577a67dbfd95e81f94ce029014e"
 // const authToken="023db2b667cb25c4e98159f882590bdb"
 
+ console.log(accountSid,authToken,"prueva de ejecucion del crontollrr para clientes")
+
 const client = twilio(accountSid, authToken);
 
-
-cron.schedule('25 19 * * *', async () => {
+cron.schedule('29 19 * * *', async () => {
     try {
  console.log(accountSid,authToken,"prueva de ejecucion del cron para clientes")
       const today = new Date();
